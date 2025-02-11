65353c1afbf28db379357d88929b33f23b817ee1
hassim506 <hassimiou061@gmail.com>
2025-02-11 15:11:53 +0100

    Ajout du hamburger pour le Mobile NAv

--- components/ui/MobileNav.tsx
+++ components/ui/MobileNav.tsx
@@ -1,3 +1,4 @@
+"use client"
 import {
     Sheet,
     SheetContent,
@@ -6,13 +7,22 @@ import {
     SheetTitle,
     SheetTrigger,
   } from "@/components/ui/sheet"
+import Image from "next/image"
   
 
 const MobileNav = ({user}: MobileNavProps) => {
   return (
     <section className="w-full max-w-[264px]">
         <Sheet>
-  <SheetTrigger>Open</SheetTrigger>
+  <SheetTrigger>
+<Image 
+src="/icons/hamburger.svg"
+width={30}
+height={30}
+alt="menu"
+className="cursor-pointer"
+/>
+  </SheetTrigger>
   <SheetContent>
     <SheetHeader>
       <SheetTitle>Are you absolutely sure?</SheetTitle>
