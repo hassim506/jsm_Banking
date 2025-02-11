1b9468c5c36951c5509f8fcb983cb798f4701064
hassim506 <hassimiou061@gmail.com>
2025-02-11 13:28:00 +0100

    ajout du sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,15 +1,19 @@
+import Sidebar from "@/components/ui/Sidebar";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+    const loggedIn = {firstName: 'Hassim', lastName: 'Diallo'};  
+
+
   return (
-    <html lang="en">
-     <main>
-        SIDEBAR
+    
+     <main className="flex h-screen w-full">
+      <Sidebar user={loggedIn  }/>
         {children }
      </main>
-    </html>
+   
   );
 }
